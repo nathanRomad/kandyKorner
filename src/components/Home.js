ddb7c4cb5d19d39e6dba9d677ba8ba0e751cf889
nathanRomad <nathan.a.hamilton@gmail.com>
2021-03-05 09:54:37 -0600

    feat: NavBar added / locations added / style: some css applied

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,10 @@
 import React from "react";
+import "./Home.css";
 
 export const HomeCard = ({ }) => (
     <section className="home">
         <h2>Kandy Korner</h2>
         <small>...enjoy the little things!</small>
-
         <address>
             <div>Visit us in Nashville</div>
             <div>1,000,000 Candies Drive</div>
--- src/components/locations/Location.js
+++ src/components/locations/Location.js
@@ -1,4 +1,5 @@
 import React from "react"
+import "./Locations.css"
 
 export const Location = ({ locations }) => (
     <section className="location">
